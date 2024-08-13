5b2284b1b6fb73d7269f32455dbca9825257b8cb
Dana R Abdulhaq <116758444+danarabdulhaq@users.noreply.github.com>
2024-08-13 14:31:12 +0300

    Time: 2 ms (49.27%) | Memory: 7.5 MB (42.98%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,20 @@
+
 class Solution {
 public:
-    int climbStairs(int n) {
-      if (n == 0 || n == 1) {
-            return 1;
-        }
-
-        vector<int> dp(n+1);
-        dp[0] = dp[1] = 1;
-        
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+  int climbStairs(int n) {
+    if (n == 0 || n == 1) {
+      return 1;
     }
+
+    int v1 = 1;
+    int v2 = 1;
+    int val;
+    for (int i = 2; i <= n; i++) {
+      val = v1 + v2;
+      v1 = v2;
+      v2 = val;
+      // dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return val;
+  }
 };
